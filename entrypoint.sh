40bca12e3305be671727a942a323417eb5041cf1
Vlad-Andrei Erculescu <vlad.erculescu@hotmail.com>
2021-02-21 16:48:53 +0000

    Added eco for start steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
-echo "Cloning $REPO ..."
+echo "Cleaning directory ..."
 rm -rf /usr/share/nginx/html/*
 rm -rf /usr/share/nginx/html/.git
+
+echo "Cloning $REPO ..."
 git clone $REPO /usr/share/nginx/html/
-echo "Done cloning files!"
+
 echo "Starting server ..."
 
 nginx -g 'daemon off;'
